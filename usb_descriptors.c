3c4c95fbd48d11923108e30ed25f86cf98aae7db
besthathacker <aiden123o@outlook.com>
2025-09-11 16:28:17 -0700

    Add files via upload

--- usb_descriptors.c
+++ usb_descriptors.c
@@ -37,6 +37,25 @@ uint8_t const *tud_hid_descriptor_report_cb(uint8_t instance) {
     return desc_hid_report;
 }
 
+// Configuration descriptor
+enum {
+    ITF_NUM_HID,
+    ITF_NUM_TOTAL
+};
+
+#define CONFIG_TOTAL_LEN    (TUD_CONFIG_DESC_LEN + TUD_HID_DESC_LEN)
+
+uint8_t const desc_configuration[] = {
+    TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, 0x00, 100),
+    TUD_HID_DESCRIPTOR(ITF_NUM_HID, 0, HID_ITF_PROTOCOL_NONE,
+                       sizeof(desc_hid_report), 0x81, 16, 10)
+};
+
+uint8_t const * tud_descriptor_configuration_cb(uint8_t index) {
+    (void) index;
+    return desc_configuration;
+}
+
 // Strings
 char const* string_desc_arr[] = {
     (const char[]) {0x09, 0x04},
